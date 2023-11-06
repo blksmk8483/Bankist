1b54b142b2f7dc60b87cdc0decb3729f4fa40dc6
Brandon Knight <knightbrandon1984@gmail.com>
2023-11-05 21:15:48 -0600

    setup for the movements section

--- script.js
+++ script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -79,145 +97,145 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // ==========================================
 // ==========================================
 // ==========================================
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('SIMPLE ARRAY METHODS');
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('SIMPLE ARRAY METHODS');
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// SLICE - does not affect the original array
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
+// // SLICE - does not affect the original array
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+// console.log([...arr]);
 
-// SPLICE - does affect the original array
-// console.log(arr.splice(2));
-arr.splice(-1);
-arr.splice(1, 2);
-console.log(arr);
+// // SPLICE - does affect the original array
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// arr.splice(1, 2);
+// console.log(arr);
 
-// REVERSE - does affect the original array
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'j'];
-console.log(arr2.reverse());
-console.log(arr2);
+// // REVERSE - does affect the original array
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'j'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// CONCAT - does affect the original array
-const letters = arr.concat(arr2);
-console.log(letters);
+// // CONCAT - does affect the original array
+// const letters = arr.concat(arr2);
+// console.log(letters);
 
-// or we can use the spread operator
-console.log([...arr, ...arr2]);
+// // or we can use the spread operator
+// console.log([...arr, ...arr2]);
 
-// JOIN
-console.log(letters.join(' - '));
+// // JOIN
+// console.log(letters.join(' - '));
 
-// ==========================================
-// ==========================================
-// ==========================================
-// ==========================================
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('THE NEW AT METHOD');
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('THE NEW AT METHOD');
 
-const arr3 = [23, 11, 64];
-console.log(arr3[0]);
-console.log(arr3.at(0));
+// const arr3 = [23, 11, 64];
+// console.log(arr3[0]);
+// console.log(arr3.at(0));
 
-// getting last array element
-console.log(arr3[arr3.length - 1]);
-console.log(arr3.slice(-1)[0]);
+// // getting last array element
+// console.log(arr3[arr3.length - 1]);
+// console.log(arr3.slice(-1)[0]);
 
-console.log(arr3.at(-1));
+// console.log(arr3.at(-1));
 
-console.log('brandon'.at(0));
-console.log('brandon'.at(-1));
+// console.log('brandon'.at(0));
+// console.log('brandon'.at(-1));
 
-// ==========================================
-// ==========================================
-// ==========================================
-// ==========================================
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('LOOPING ARRAYS: forEach');
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('LOOPING ARRAYS: forEach');
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}.`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-  }
-}
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}.`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
 
-console.log('========= forEach method ========');
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited $${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew $${Math.abs(mov)}`);
-  }
-});
+// console.log('========= forEach method ========');
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited $${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew $${Math.abs(mov)}`);
+//   }
+// });
 
-const practiceArray = [123, 234, 423, 867, 5, 30, 9];
-practiceArray.forEach((yourmom, is) => console.log(`${is}: ${yourmom}`));
+// const practiceArray = [123, 234, 423, 867, 5, 30, 9];
+// practiceArray.forEach((yourmom, is) => console.log(`${is}: ${yourmom}`));
 
-// ==========================================
-// ==========================================
-// ==========================================
-// ==========================================
-console.log('================================');
-console.log('================================');
-console.log('================================');
-console.log('forEach WITH MAPS and SETS');
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// // ==========================================
+// console.log('================================');
+// console.log('================================');
+// console.log('================================');
+// console.log('forEach WITH MAPS and SETS');
 
-// a new map holds a key-value pair
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// // a new map holds a key-value pair
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-// longhand
-console.log('=====lonhand vs.=====');
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+// // longhand
+// console.log('=====lonhand vs.=====');
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
 
-console.log('=====arrow function vs.=====');
+// console.log('=====arrow function vs.=====');
 
-currencies.forEach((yourmom, hey) => console.log(`${hey}: ${yourmom}`));
-// yourmom = the value so in currencies that is 'United States dollar'
-// hey = the key of each value
+// currencies.forEach((yourmom, hey) => console.log(`${hey}: ${yourmom}`));
+// // yourmom = the value so in currencies that is 'United States dollar'
+// // hey = the key of each value
 
-// With a set
-const currenciesUnique = new Set(['USD', 'GDP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
+// // With a set
+// const currenciesUnique = new Set(['USD', 'GDP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
 
-currenciesUnique.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+// currenciesUnique.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
 
-const set1 = new Set([1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 2, 2]);
+// const set1 = new Set([1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 2, 2]);
 
-console.log(set1.has(1));
-// Expected output: true
+// console.log(set1.has(1));
+// // Expected output: true
 
-console.log(set1.has(5));
-// Expected output: true
+// console.log(set1.has(5));
+// // Expected output: true
 
-console.log(set1.has(6));
-// Expected output: false
+// console.log(set1.has(6));
+// // Expected output: false
 
-console.log(...set1);
-// 1 2 3 4 5
+// console.log(...set1);
+// // 1 2 3 4 5
