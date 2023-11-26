d3ff10f312ce67236cb77b8c967ca298193de776
Brandon Knight <knightbrandon1984@gmail.com>
2023-11-25 23:36:55 -0600

    The Remainder operator

--- script.js
+++ script.js
@@ -1157,3 +1157,40 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// // // // ==========================================
+// // // // ==========================================
+// // // // ==========================================
+// // // // ==========================================
+// // // console.log('================================');
+// // // console.log('================================');
+// // // console.log('================================');
+// // // console.log('THE REMAINDER OPERATOR');
+
+// REMAINDER OPERATOR is the % symbol
+
+console.log(5 % 2); // 5 = 2 * 2 + 1
+console.log(5 / 2); // 2.5
+
+console.log(8 % 3); // 8 = 2 * 3 + 2
+console.log(8 / 3); // 2.66
+
+console.log(6 % 2); // 0
+console.log(6 / 2); // 3
+
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8)); // true
+console.log(isEven(23)); // false
+console.log(isEven(514)); // true
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
