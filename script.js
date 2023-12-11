c24fcc3401f05f62f27cca7ee785ca389ca4023b
Brandon Knight <knightbrandon1984@gmail.com>
2023-12-11 13:31:27 -0600

    custom dates added to the app

--- script.js
+++ script.js
@@ -55,9 +55,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-12-08T17:01:17.194Z',
+    '2023-12-10T23:36:17.929Z',
+    '2023-12-11T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -111,6 +111,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// -------------- FUNCTIONS ----------------
+
+const formateMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  }
+};
+
 // THIS DISPLAYS THE DEPOSITS AND WITHDRAWALS
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -123,11 +144,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
-    const displayDate = `${month}/${day}/${year}`;
+    const displayDate = formateMovementDate(date);
 
     const html = `
     <div class="movements__row">
@@ -1313,3 +1330,22 @@ const future = new Date(2037, 10, 19, 15, 23);
 console.log(future);
 
 console.log(Date.now());
+
+// ==========================================
+// ==========================================
+// ==========================================
+// ==========================================
+console.log('================================');
+console.log('================================');
+console.log('================================');
+console.log('DATES AND TIMES');
+
+const future1 = new Date(2037, 10, 19, 15, 23);
+console.log(Number(future1));
+console.log(+future1); // same thing
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
